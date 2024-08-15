1128ecdd9b84f51962e65b192f652b7cd032603d
liangshuang <liangshuang@eswincomputing.com>
2024-08-15 13:53:08 +0800

    add GPL-2.0 license.

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: GPL-2.0
 /*
- * config file for nsig
+ * config file for nsign
  *
  * Copyright 2024 Beijing ESWIN Computing Technology Co., Ltd.
  *   Authors:
--- src/esw_build_signature.h
+++ src/esw_build_signature.h
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+﻿// SPDX-License-Identifier: GPL-2.0
 /*
  * This file mainly describes the data structure
  *
